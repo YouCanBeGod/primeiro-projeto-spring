bf937aa964d1a5987054dc5a7aa03e3cffa928a1
YouCanBeGod <dallasta.amos@gmail.com>
2023-02-13 20:21:02 -0300

    Criação de Classes + Primeiro Bean

---
+++ src/main/java/dio/primerioprojetospring/AppConfig.java
@@ -0,0 +1,14 @@
+package dio.primerioprojetospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Livro getLivro() {
+        return new Livro();
+    }
+    // <bean id="livro" class="com.springbeans.Livro"/>
+}
