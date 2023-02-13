7a08b62e6b0ac18fcbf2323aeca5d0dcea67e54d
YouCanBeGod <dallasta.amos@gmail.com>
2023-02-13 20:03:49 -0300

    First Commit - Criação do Projeto

---
+++ src/main/java/dio/primerioprojetospring/PrimerioProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.primerioprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerioProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerioProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/primerioprojetospring/PrimerioProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.primerioprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerioProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
