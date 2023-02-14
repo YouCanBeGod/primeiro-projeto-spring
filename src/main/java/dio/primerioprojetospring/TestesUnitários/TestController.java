d08f095a9c62f258610e42f6ac545ee0468cbc3a
YouCanBeGod <dallasta.amos@gmail.com>
2023-02-14 20:37:45 -0300

    Implementação de teste unitário

---
+++ src/main/java/dio/primerioprojetospring/TestesUnitários/TestController.java
@@ -0,0 +1,13 @@
+package dio.primerioprojetospring.TestesUnitários;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String saudacao(String nome) {
+        return String.format("Bem-vindo, %s", nome);
+    }
+}
